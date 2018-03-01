f38957d184386093efc6dcf4ccd7f21b572d1a45
Tran Quoc Trung <tranquoctrung.1610@gmail.com>
2018-03-01 20:23:49 +0700

    fixes upload storage destination

--- server/routes/files.js
+++ server/routes/files.js
@@ -7,12 +7,12 @@ var storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null,'./uploads')
     }
-//     ,
-//   filename: function (req, file, cb) {
-//     cb(null, file.fieldname + '-' + Date.now())
-//   }
+     ,
+   filename: function (req, file, cb) {
+     cb(null, file.fieldname + '-' + Date.now())
+   }
 })
-var upload = multer() 
+var upload = multer({storage: storage}) 
 
 var router = express.Router();
 
